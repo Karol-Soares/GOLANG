610ab7cac9eac869e234abe06448b93005c34206
Karol-Soares <karolina.soares@sumup.com>
2023-09-29 18:49:39 -0300

    [hackday] Creating deposit method with multiples returns

--- orientacao_a_objetos/main.go
+++ orientacao_a_objetos/main.go
@@ -29,14 +29,23 @@ func (conta *ContaCorrente) Sacar(valorDoSaque float64) string {
 	}
 }
 
+// Criando metodo depositar com múltiplos retornos
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
+	if valorDoDeposito > 0 {
+		c.saldo += valorDoDeposito
+		return "deposito realizado com sucesso", c.saldo
+	} else {
+		return "valor do deposito menor que zero", c.saldo
+	}
+}
+
 func main() {
 	contaDoRoronoa := ContaCorrente{}
 	contaDoRoronoa.titular = "Roronoa"
 	contaDoRoronoa.saldo = 500
 
 	fmt.Println(contaDoRoronoa.saldo)
+	status, valor := contaDoRoronoa.Depositar(2000)
+	fmt.Println(status, valor)
 
-	fmt.Println(contaDoRoronoa.Sacar(400))
-
-	fmt.Println(contaDoRoronoa.saldo)
 }
